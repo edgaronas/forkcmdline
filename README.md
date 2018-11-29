f6dfcc4f0e212aa143f921682bd1d82a4d4c2ebd
Edgaras Sakuras <edgaronas@yahoo.com>
2018-11-29 03:38:03 +0200

    beta

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
-# forkcmdline
-Small Perl script to run command lines in parallel.
+# NAME
+
+**forkcmdline** - run Perl `system` function with `Parallel::ForkManager`
+from input or file.
+
+# SYNOPSIS
+
+    forkcmdline [OPTIONS] CMDLINE [CMDLINE ...]
+    forkcmdline [OPTIONS] --read CMDFILE [CMDFILE ...]
+
+# DESCRIPTION
+
+## Dependencies
+
+Perl standard modules: `strict`, `warnings` (with `FATAL` warnings).
+
+`Parallel::ForkManager` - for Perl fork managing.
+
+`Time::HiRes` - for precise elapsed time.
+
+`Getopt::Long` - for handling command line options.
+
+## Options
+
+    -a, --writeall=FILEBASE 
+                write exitcodes, stdout and stderr of cmdlines to files with fixed base name (or path) 
+                and added fork number suffix: .exit.1 and .stdout.1 and .stderr.1 and so on
+                default: do not write
+                warning: redefines other write options
+    -c, --checkexit=INTEGER[,INTEGER[,...]]
+                check if every command ended with any exitcode from list, 
+                generate error if not, default: do not check
+    -e, --writestderr=FILEBASE   
+                write stderr of cmdlines to files with fixed base name (or path) 
+                and added fork number suffix: .stderr.1 and so on
+                default: do not write
+    -f, --forks=INTEGER     
+            number of forks to run in parallel, 
+            possible values:
+                1 - count number automatically (default)
+                0 - do not fork and run consistently (for debug)
+                2 and so on - fixed number of forks
+    -h, --help              
+            show minimal help end exit
+    -o, --writestdout=FILEBASE   
+                write stdout of cmdlines to files with fixed base name (or path) 
+                and added fork number suffix: .stdout.1 and so on
+                default: do not write
+    -q, --quiet             
+            do not chat while working (no prints or warnings)
+    -r, --read
+                read and run command lines from files, 
+                one line = one command,
+                empty and space only lines are ignored
+    -s, --sleep=FLOAT       
+                sleep time in seconds of pseudo-blocking calls, default: 0.01
+    -x, --writeexit=FILEBASE   
+                write exitcodes of cmdlines to files with fixed base name (or path) 
+                and added fork number suffix: .exit.1 and so on
+                default: do not write
+
+## Getting fork number
+
+If the number of forks is not set by option, it is retrieved from:
+
+1) environmental variable `NUMBER_OF_PROCESSORS`, targeting Windows-like OSes;
+
+2) `nproc` utility, targeting Unix-like OSes.
+
+If above steps fail, it is simply set to 4.
+
+Finally, it is multiplied by 2 targeting 2 forks per core 
+(this assumption made by script author after some modest tests).
+
+The number of forks is reduced to the number of cmdlines if later is smaller.
+
+It is set to zero, if there is only one cmdline to run.
+
+All forks are waited to end. If fork is lost, warning is generated, 
+though usually it does not mean that process has failed. 
+So additional analysis should be made by script caller, 
+e.g..: check result file of `cmdline`, check existence of `writeexit` files and so on.
+
+If `--quiet` is not used, end time is printed to STDOUT.
+
+# TODO
+
+\- Tests on Windows.
+
+\- Are there any reasons to hard-code checking existence of `writeexit` and so on files?
+
+\- Reproduce situations when `$exitcode` in `run_on_finish`
+is less reliable than from `$datastructref`.
+
+\- Are there any reasons not to check core dump from `run_on_finish`?
+
+\- Reproduce and test situations when fork is lost with warning (e.g. in Windows).
+Is `writeexit` and so on files still created?
+
+# LICENSE
+
+The MIT License. More info: [https://opensource.org/licenses/MIT](https://opensource.org/licenses/MIT).
+
+# AUTHOR
+
+Edgaras Sakuras - [edgaronas@yahoo.com](mailto:edgaronas@yahoo.com)
+
+2018-11-29
---
+++ forkcmdline.pl
@@ -0,0 +1,315 @@
+#!/usr/bin/perl
+
+=pod
+
+=head1 NAME
+
+B<forkcmdline> - run Perl C<system> function with C<Parallel::ForkManager>
+from input or file.
+
+=head1 SYNOPSIS
+
+    forkcmdline [OPTIONS] CMDLINE [CMDLINE ...]
+    forkcmdline [OPTIONS] --read CMDFILE [CMDFILE ...]
+
+=head1 DESCRIPTION
+
+=head2 Dependencies
+
+Perl standard modules: C<strict>, C<warnings> (with C<FATAL> warnings).
+
+C<Parallel::ForkManager> - for Perl fork managing.
+
+C<Time::HiRes> - for precise elapsed time.
+
+C<Getopt::Long> - for handling command line options.
+
+=cut
+
+use strict;
+use warnings 'FATAL' => 'all';
+use Parallel::ForkManager ();
+use Time::HiRes ();
+use Getopt::Long ();
+
+$|=1;
+
+=pod
+
+=head2 Options
+
+    -a, --writeall=FILEBASE 
+                write exitcodes, stdout and stderr of cmdlines to files with fixed base name (or path) 
+                and added fork number suffix: .exit.1 and .stdout.1 and .stderr.1 and so on
+                default: do not write
+                warning: redefines other write options
+    -c, --checkexit=INTEGER[,INTEGER[,...]]
+                check if every command ended with any exitcode from list, 
+                generate error if not, default: do not check
+    -e, --writestderr=FILEBASE   
+                write stderr of cmdlines to files with fixed base name (or path) 
+                and added fork number suffix: .stderr.1 and so on
+                default: do not write
+    -f, --forks=INTEGER     
+            number of forks to run in parallel, 
+            possible values:
+                1 - count number automatically (default)
+                0 - do not fork and run consistently (for debug)
+                2 and so on - fixed number of forks
+    -h, --help              
+            show minimal help end exit
+    -o, --writestdout=FILEBASE   
+                write stdout of cmdlines to files with fixed base name (or path) 
+                and added fork number suffix: .stdout.1 and so on
+                default: do not write
+    -q, --quiet             
+            do not chat while working (no prints or warnings)
+    -r, --read
+                read and run command lines from files, 
+                one line = one command,
+                empty and space only lines are ignored
+    -s, --sleep=FLOAT       
+                sleep time in seconds of pseudo-blocking calls, default: 0.01
+    -x, --writeexit=FILEBASE   
+                write exitcodes of cmdlines to files with fixed base name (or path) 
+                and added fork number suffix: .exit.1 and so on
+                default: do not write
+
+=cut
+
+my $checkexit='';
+my $forks = 1;
+my $writeall='';
+my $writeexit='';
+my $writestderr='';
+my $writestdout='';
+my $quiet = 0;
+my $read = 0;
+my $sleep = 0.01;
+
+Getopt::Long::GetOptions(
+    'writeall|a=s'      =>  \$writeall,
+    'checkexit|c=s'     =>  \$checkexit,
+    'writestderr|e=s'   =>  \$writestderr,
+    'forks|f=i'         =>  \$forks,
+    'help|h'            =>  sub { 
+                                system("perldoc $0") == 0 or 
+                                    die("ERROR: failed to run 'perldoc' on file '$0' for help: $?, $!\n");
+                                exit;
+                            },
+    'writestdout|o=s'   =>  \$writestdout,
+    'quiet|q'           =>  \$quiet,
+    'read|r'            =>  \$read,
+    'sleep|s=f'         =>  \$sleep,
+    'writeexit|x=s'     =>  \$writeexit,
+) or die("ERROR: failed processing command line arguments\n");
+
+die("ERROR: missing arguments... use --help option for usage!\n") unless(@ARGV);
+
+if($writeall ne ''){
+    $writeexit = $writeall;
+    $writestdout = $writeall;
+    $writestderr = $writeall;
+}
+
+my @CMDS;
+if($read){
+    foreach my $argv (@ARGV){
+        my $rownum = 0;
+        open(my $fh, '<', $argv) or die("ERROR: failed to open file '$argv' for reading: $?, $!\n");
+        while(my $row = <$fh>){
+            $rownum++;
+            $row=~s/^\s+|\s+$//s;
+            if(length($row)>0){
+                push @CMDS, $row;
+            }
+            else {
+                warn('WARNING: '.ordinal_suffix($rownum).' row is empty or space only in: '.
+                    $argv."\n") unless($quiet);
+            }
+        }
+    }
+}
+else {
+    @CMDS=@ARGV;
+}
+
+=head2 Getting fork number
+
+If the number of forks is not set by option, it is retrieved from:
+
+1) environmental variable C<NUMBER_OF_PROCESSORS>, targeting Windows-like OSes;
+
+2) C<nproc> utility, targeting Unix-like OSes.
+
+If above steps fail, it is simply set to 4.
+
+Finally, it is multiplied by 2 targeting 2 forks per core 
+(this assumption made by script author after some modest tests).
+
+The number of forks is reduced to the number of cmdlines if later is smaller.
+
+It is set to zero, if there is only one cmdline to run.
+
+=cut
+
+if($forks == 1){
+    $forks = 4;
+    if($ENV{'NUMBER_OF_PROCESSORS'}){
+        $forks = $ENV{'NUMBER_OF_PROCESSORS'};
+    }
+    else {
+        my $nproc;
+        eval { $nproc = `nproc`; };
+        if(defined $nproc){
+            $nproc=~s/\s+$//gs;
+            $forks = $nproc;
+        }
+        else {
+            warn("WARNING: failed to run nproc utility in current OS: $^O\n".
+                "number of forks set to: $forks\n") unless($quiet);
+        }
+    }
+    $forks*=2;
+}
+
+$forks = $#CMDS+1 if($#CMDS<$forks);
+if($#CMDS==0){
+    warn("WARNING: no forking, only one element found...\n") unless($quiet);
+    $forks = 0;
+}
+
+my $pm = Parallel::ForkManager->new($forks);
+$pm->set_waitpid_blocking_sleep($sleep);
+
+unless($forks == 0){
+    $pm->run_on_finish(
+        sub {
+            my ($pid, $exitcode, $procid, $exitsignal, $coredump, $datastructref) = @_;
+            if(defined $$datastructref){
+                write_exit($procid, $$datastructref) if($writeexit ne '');
+                check_exit($procid, $$datastructref) if($checkexit ne '');
+            }
+            else {
+                die("ERROR: failed to get exit code ref for writing after ".
+                    ordinal_suffix($procid).' cmdline: '.$CMDS[$procid-1]."\n") if($writeexit ne '');
+                check_exit($procid, $exitcode) if($checkexit ne '');
+            }
+            die('ERROR: core dump for proc (pid=$pid) after '.ordinal_suffix($procid).' cmdline: '.$CMDS[$procid-1]."\n") if($coredump);
+        }
+    );
+}
+
+my $start_time;
+unless($quiet){
+    $start_time=[Time::HiRes::gettimeofday()];
+    print("PARALLEL FORKS $forks FOR CMDLINES ".($#CMDS+1).'..');
+}
+
+DATA_LOOP:
+for(my $c=0; $c<=$#CMDS; $c++){
+    my $procid=$c+1;
+    my $pid = $pm->start($procid) and next DATA_LOOP;
+    print('.'.$procid) unless($quiet);
+    my $cmdout='';
+    $cmdout=" 1> $writestdout.stdout.$procid" if($writestdout ne '');
+    my $cmderr='';
+    $cmderr=" 2> $writestderr.stderr.$procid" if($writestderr ne '');
+    my $exit = system($CMDS[$c].$cmdout.$cmderr);
+    if($forks==0){
+        write_exit($procid, $exit) if($writeexit ne '');
+        check_exit($procid, $exit) if($checkexit ne '');
+    }
+    if($checkexit.$writeexit ne ''){
+        $pm->finish(undef, \$exit);
+    }
+    else {
+        $pm->finish;
+    }
+}
+
+=pod
+
+All forks are waited to end. If fork is lost, warning is generated, 
+though usually it does not mean that process has failed. 
+So additional analysis should be made by script caller, 
+e.g..: check result file of C<cmdline>, check existence of C<writeexit> files and so on.
+
+If C<--quiet> is not used, end time is printed to STDOUT.
+
+=cut
+
+print('...waiting') unless($quiet);
+$pm->wait_all_children;
+print('...DONE in ', Time::HiRes::tv_interval($start_time, 
+    [Time::HiRes::gettimeofday()]), "s\n") unless($quiet);
+
+### SUBS ### 
+
+sub check_exit {
+    my $procid = shift(@_);
+    my $exit = shift(@_);
+    my @CHK = split(/,\s*/s, $checkexit);
+    my $eok = 0;
+    foreach my $chk (@CHK){
+        if($chk == $exit){
+            $eok = 1;
+            last;
+        }
+    }
+    die('ERROR: '.ordinal_suffix($procid)." cmdline '".$CMDS[$procid-1]."' exited with $exit, ".
+        "though expected: ".join(' or ', @CHK)."\n") unless($eok);
+}
+sub write_exit {
+    my $procid = shift(@_);
+    my $exit = shift(@_);
+    open(my $fh, '>', "$writeexit.exit.$procid") or 
+        die("ERROR: failed to open file '$writeexit.exit.$procid' for writing: $?, $!\n");
+    print $fh $exit;
+    close($fh);
+}
+
+sub ordinal_suffix {
+    my $number=shift(@_);
+    if($number =~ /(?<!1)1$/){
+        return $number.'st';
+    } 
+    elsif($number =~ /(?<!1)2$/){
+        return $number.'nd';
+    }
+    elsif($number =~ /(?<!1)3$/){
+        return $number.'rd';
+    }
+    else {
+        return $number.'th';
+    }
+}
+
+=pod
+
+=head1 TODO
+
+- Tests on Windows.
+
+- Are there any reasons to hard-code checking existence of C<writeexit> and so on files?
+
+- Reproduce situations when C<$exitcode> in C<run_on_finish>
+is less reliable than from C<$datastructref>.
+
+- Are there any reasons not to check core dump from C<run_on_finish>?
+
+- Reproduce and test situations when fork is lost with warning (e.g. in Windows).
+Is C<writeexit> and so on files still created?
+
+=head1 LICENSE
+
+The MIT License. More info: L<https://opensource.org/licenses/MIT>.
+
+=head1 AUTHOR
+
+Edgaras Sakuras - L<edgaronas@yahoo.com|mailto:edgaronas@yahoo.com>
+
+2018-11-29
+
+=cut
+
